1729b6eeef73b57973b07f6471d6f3935c74c1c4
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2023-12-10 23:03:26 +0100

    Added navigation with goRouter

---
+++ lib/utils/navigation/app_router_paths.dart
@@ -0,0 +1,7 @@
+class AppRouterPaths {
+  static const home = '/home';
+  static const auth = '/auth';
+  static const login = '/login';
+  static const sign_up = '/sign-up';
+  static const receipt = '/receipt';
+}
---
+++ lib/utils/navigation/router.dart
@@ -0,0 +1,49 @@
+import 'package:budget_master/screens/login_screen.dart';
+import 'package:budget_master/screens/receipt_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../screens/auth_screen.dart';
+import '../../screens/home_screen.dart';
+import 'app_router_paths.dart';
+
+class AppRouter {
+  static final router = GoRouter(
+    //refreshListenable: RouterNotifier(),
+    //redirect: RouterNotifier().redirect,
+    debugLogDiagnostics: true,
+    initialLocation: AppRouterPaths.auth,
+    routes: [
+      GoRoute(
+          name: 'home',
+          path: AppRouterPaths.home,
+          builder: (BuildContext context, GoRouterState state) {
+            return const HomeScreen();
+          }),
+      GoRoute(
+          name: 'auth',
+          path: AppRouterPaths.auth,
+          builder: (BuildContext context, GoRouterState state) {
+            return const AuthScreen();
+          }),
+      GoRoute(
+          name: 'login',
+          path: AppRouterPaths.login,
+          builder: (BuildContext context, GoRouterState state) {
+            return const LoginScreen();
+          }),
+      GoRoute(
+          name: 'sign-up',
+          path: AppRouterPaths.sign_up,
+          builder: (BuildContext context, GoRouterState state) {
+            return const LoginScreen();
+          }),
+      GoRoute(
+          name: 'receipt',
+          path: AppRouterPaths.receipt,
+          builder: (BuildContext context, GoRouterState state) {
+            return const ReceiptScreen();
+          }),
+    ],
+  );
+}
