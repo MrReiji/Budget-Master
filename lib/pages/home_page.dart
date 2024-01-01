dffd83a815b13d459e9a3969a01283c25fae5112
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2024-01-01 18:18:12 +0100

    Created pages list, made home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,95 @@
+import 'package:budget_master/utils/constants.dart';
+import 'package:budget_master/utils/firebase/getCurrentUsername.dart';
+import 'package:budget_master/widgets/latest_receipts.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: kToolbarHeight + 20,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FutureBuilder<String?>(
+                          future: getCurrentUsername(),
+                          builder: (context, snapshot) {
+                            return RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: "Welcome Back,\n",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleLarge
+                                        ?.copyWith(
+                                          color: Colors.white,
+                                        ),
+                                  ),
+                                  TextSpan(
+                                    text: "${snapshot.data ?? 'User'}!",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleLarge
+                                        ?.copyWith(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                  )
+                                ],
+                              ),
+                            );
+                          }),
+                      Image.asset(
+                        "assets/profile.png",
+                        scale: 6,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Container(
+              width: double.infinity,
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height - 200.0,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+                color: Constants.scaffoldBackgroundColor,
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 24.0,
+              ),
+              child: LatestReceipts(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:budget_master/pages/Pages_list.dart';
+import 'package:budget_master/pages/home_page.dart';
 import 'package:budget_master/utils/firebase/getCurrentUsername.dart';
+import 'package:budget_master/utils/navigation/app_router_paths.dart';
 import 'package:budget_master/widgets/latest_receipts.dart';
 import 'package:budget_master/widgets/navbar_items.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
@@ -31,101 +34,9 @@ class _HomeScreenState extends State<HomeScreen> {
         },
       ),
       backgroundColor: Constants.primaryColor,
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: kToolbarHeight,
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16.0,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        context.pop();
-                      },
-                      child: Icon(
-                        Icons.keyboard_backspace_rounded,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        FutureBuilder<String?>(
-                            future: getCurrentUsername(),
-                            builder: (context, snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return CircularProgressIndicator();
-                              } else {
-                                return RichText(
-                                  text: TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: "Welcome Back,\n",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleLarge
-                                            ?.copyWith(
-                                              color: Colors.white,
-                                            ),
-                                      ),
-                                      TextSpan(
-                                        text: "${snapshot.data ?? 'User'}!",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .titleLarge
-                                            ?.copyWith(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.w600,
-                                            ),
-                                      )
-                                    ],
-                                  ),
-                                );
-                              }
-                            }),
-                        Image.asset(
-                          "assets/profile.png",
-                          scale: 6,
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              Container(
-                width: double.infinity,
-                constraints: BoxConstraints(
-                  minHeight: MediaQuery.of(context).size.height - 200.0,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                  ),
-                  color: Constants.scaffoldBackgroundColor,
-                ),
-                padding: EdgeInsets.symmetric(
-                  vertical: 24.0,
-                ),
-                child: LatestReceipts(),
-              )
-            ],
-          ),
-        ),
+      body: IndexedStack(
+        index: activeIndex,
+        children: pagesList,
       ),
     );
   }
