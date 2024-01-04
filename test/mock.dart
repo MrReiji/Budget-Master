608e2049d4b3e5145cd03249db5b278b50436785
SX-7 <sn1.test.preria.2002@gmail.com>
2024-01-04 01:52:10 +0100

    Added relevant credits

--- test/mock.dart
+++ test/mock.dart
@@ -1,3 +1,5 @@
+// https://github.com/firebase/flutterfire/blob/master/packages/firebase_auth/firebase_auth/test/mock.dart
+
 // Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
