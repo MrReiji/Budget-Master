d600d1cd8ee445b59b956342f48f8b26423a33b0
SX-7 <sn1.test.preria.2002@gmail.com>
2024-01-04 02:51:30 +0100

    Solved edge test case in relation to screen sizes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:budget_master/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -15,12 +16,30 @@ void main() {
   });
 
   // Define a widget test
-  testWidgets('Home screen welcome message test', (WidgetTester tester) async {
+  testWidgets('Auth screen UI test', (WidgetTester tester) async {
     // Build the app and trigger a frame
 
     await tester.pumpWidget(const MyApp());
 
     // Verify that the welcome message is present on the screen
     expect(find.text("Welcome to Budget Master!"), findsOneWidget);
+    expect(find.text("Log In"), findsOneWidget);
+    expect(find.text("Create an Account"), findsOneWidget);
   });
+
+  testWidgets('Auth screen navigation test', (WidgetTester tester) async
+  { 
+    WidgetController.hitTestWarningShouldBeFatal = true;
+    tester.view.physicalSize = const Size(2400, 1080);
+    tester.view.devicePixelRatio = 1.0;
+    
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.bySemanticsLabel("Log In"));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(LoginScreen), findsOne);
+  });
+
 }
