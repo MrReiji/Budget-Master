93a3f624b4697d154ec33b27c3958b0daabf32f2
SX-7 <sn1.test.preria.2002@gmail.com>
2024-01-08 05:02:22 +0100

    Testing if it works remotely

--- test/firebase_test.dart
+++ test/firebase_test.dart
@@ -1,7 +1,11 @@
+import 'package:budget_master/firebase_options.dart';
 import 'package:budget_master/screens/auth_screen.dart';
 import 'package:budget_master/screens/home_screen.dart';
 import 'package:budget_master/screens/login_screen.dart';
 import 'package:budget_master/widgets/input_widget.dart';
+import 'package:budget_master/widgets/loading_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,13 +16,14 @@ import './mock.dart';
 void main() {
   // Mock Firebase before running any tests
   setUpAll(() async {
-    setupFirebaseAuthMocks();
     await dotenv.load(fileName: ".env");
     WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   });
 
-testWidgets('Login screen navigation test', (WidgetTester tester) async {
+  testWidgets('Login screen navigation test', (WidgetTester tester) async {
     // Fixes sizing issue in test
     WidgetController.hitTestWarningShouldBeFatal = true;
     tester.view.physicalSize = const Size(1080, 2220);
@@ -41,7 +46,8 @@ testWidgets('Login screen navigation test', (WidgetTester tester) async {
             widget.textInputType == TextInputType.visiblePassword),
         "mockmockmock");
     await tester.tap(find.bySemanticsLabel(RegExp("[Ll]og.*[Ii]n\$")));
-    await tester.pumpAndSettle(); // This *will* fail if firebase emulation is not up and runnning
-    expect(find.byType(HomeScreen), findsOne);
+    await tester
+        .pumpAndSettle(); // This *will* fail if firebase emulation is not up and runnning
+    expect(find.byType(LoadingDialog), findsOne);
   });
 }
