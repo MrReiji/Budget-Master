194b094ca02ea3b2bf7a1e4ed57b61268b43d7fd
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2024-01-15 21:29:45 +0100

    latest_receipts display based on function of snapshot

--- lib/widgets/receipts/latest_receipts.dart
+++ lib/widgets/receipts/latest_receipts.dart
@@ -8,78 +8,6 @@ import '../../constants/constants.dart';
 import 'receipt_card.dart';
 
 class LatestReceipts extends StatelessWidget {
-  // final List<Order> receipts = [
-  //   Order(
-  //     id: 507,
-  //     deliveryAddress: "New Times Square",
-  //     arrivalDate: DateTime(2020, 1, 23),
-  //     placedDate: DateTime(2020, 1, 17),
-  //     status: OrderStatus.DELIVERING,
-  //   ),
-  //   Order(
-  //     id: 536,
-  //     deliveryAddress: "Victoria Square",
-  //     arrivalDate: DateTime(2020, 1, 21),
-  //     placedDate: DateTime(2020, 1, 19),
-  //     status: OrderStatus.PICKING_UP,
-  //   ),
-  //   Order(
-  //     id: 507,
-  //     deliveryAddress: "New Times Square",
-  //     arrivalDate: DateTime(2020, 1, 23),
-  //     placedDate: DateTime(2020, 1, 17),
-  //     status: OrderStatus.DELIVERING,
-  //   ),
-  //   Order(
-  //     id: 536,
-  //     deliveryAddress: "Victoria Square",
-  //     arrivalDate: DateTime(2020, 1, 21),
-  //     placedDate: DateTime(2020, 1, 19),
-  //     status: OrderStatus.PICKING_UP,
-  //   ),
-  //   Order(
-  //     id: 507,
-  //     deliveryAddress: "New Times Square",
-  //     arrivalDate: DateTime(2020, 1, 23),
-  //     placedDate: DateTime(2020, 1, 17),
-  //     status: OrderStatus.DELIVERING,
-  //   ),
-  //   Order(
-  //     id: 536,
-  //     deliveryAddress: "Victoria Square",
-  //     arrivalDate: DateTime(2020, 1, 21),
-  //     placedDate: DateTime(2020, 1, 19),
-  //     status: OrderStatus.PICKING_UP,
-  //   ),
-  //   Order(
-  //     id: 507,
-  //     deliveryAddress: "New Times Square",
-  //     arrivalDate: DateTime(2020, 1, 23),
-  //     placedDate: DateTime(2020, 1, 17),
-  //     status: OrderStatus.DELIVERING,
-  //   ),
-  //   Order(
-  //     id: 536,
-  //     deliveryAddress: "Victoria Square",
-  //     arrivalDate: DateTime(2020, 1, 21),
-  //     placedDate: DateTime(2020, 1, 19),
-  //     status: OrderStatus.PICKING_UP,
-  //   ),
-  //   Order(
-  //     id: 507,
-  //     deliveryAddress: "New Times Square",
-  //     arrivalDate: DateTime(2020, 1, 23),
-  //     placedDate: DateTime(2020, 1, 17),
-  //     status: OrderStatus.DELIVERING,
-  //   ),
-  //   Order(
-  //     id: 536,
-  //     deliveryAddress: "Victoria Square",
-  //     arrivalDate: DateTime(2020, 1, 21),
-  //     placedDate: DateTime(2020, 1, 19),
-  //     status: OrderStatus.PICKING_UP,
-  //   )
-  // ];
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -89,14 +17,6 @@ class LatestReceipts extends StatelessWidget {
                 isEqualTo: FirebaseAuth.instance.currentUser!.uid)
             .snapshots(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final loadedReceiptsDocs = snapshot.data!.docs;
-
           return Container(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -131,70 +51,86 @@ class LatestReceipts extends StatelessWidget {
                 SizedBox(
                   height: 10.0,
                 ),
-                (!snapshot.hasData || snapshot.data!.docs.isEmpty)
-                    ? Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/empty_list_image.png',
-                              width: 300,
-                              height: 300,
-                              fit: BoxFit.cover,
-                            ),
-                            Text(
-                              "Empty list? Add your first expenses!",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ],
-                        ),
-                      )
-                    : (snapshot.hasData && snapshot.data!.docs.isNotEmpty)
-                        ? ListView.separated(
-                            shrinkWrap: true,
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 16.0,
-                              vertical: 10.0,
-                            ),
-                            physics: NeverScrollableScrollPhysics(),
-                            itemBuilder: (BuildContext context, int index) {
-                              final receipt = loadedReceiptsDocs[index].data();
-                              final List<Product> products =
-                                  List.from(receipt['product'])
-                                      .map((item) => Product(
-                                            productName: item['productName'],
-                                            price: item['price'],
-                                          ))
-                                      .toList();
-
-                              return ReceiptCard(
-                                receipt: Receipt(
-                                  receiptInputMethod: ReceiptInputMethod.MANUAL,
-                                  products: products,
-                                  storeName: receipt['storeName'],
-                                  purchaseDate: receipt['purchaseDate'],
-                                  category: receipt['category'],
-                                  description: receipt['description'],
-                                ),
-                              );
-                            },
-                            separatorBuilder:
-                                (BuildContext context, int index) {
-                              return SizedBox(
-                                height: 15.0,
-                              );
-                            },
-                            itemCount: loadedReceiptsDocs.length,
-                          )
-                        : Center(
-                            child: Text(
-                              'Something went wrong...',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ),
+                receiptsBasedOnSnapshot(snapshot),
               ],
             ),
           );
         });
   }
+}
+
+Widget receiptsBasedOnSnapshot(
+    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+  if (snapshot.connectionState == ConnectionState.waiting) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/empty_list_image.png',
+            width: 300,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          Text(
+            "Empty list? Add your first expenses!",
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+  if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+    final loadedReceiptsDocs = snapshot.data!.docs;
+
+    return ListView.separated(
+      shrinkWrap: true,
+      padding: EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 10.0,
+      ),
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        final receipt = loadedReceiptsDocs[index].data();
+
+        final List<Product> products = List.from(receipt['products'])
+            .map((item) => Product(
+                  productName: item['productName'],
+                  price: item['price'],
+                ))
+            .toList();
+
+        return ReceiptCard(
+          receipt: Receipt(
+            receiptInputMethod: ReceiptInputMethod.MANUAL,
+            products: products,
+            storeName: receipt['storeName'],
+            purchaseDate: receipt['purchaseDate'],
+            category: receipt['category'],
+            description: receipt['description'],
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return SizedBox(
+          height: 15.0,
+        );
+      },
+      itemCount: loadedReceiptsDocs.length,
+    );
+  }
+  debugPrint(snapshot.connectionState.toString());
+  debugPrint(snapshot.hasData.toString());
+  debugPrint(snapshot.data!.docs.isEmpty.toString());
+  return Center(
+    child: Text(
+      'Something went wrong...',
+      style: TextStyle(fontSize: 20),
+    ),
+  );
 }
