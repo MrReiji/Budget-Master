58cd15a6e052093f839fc38bddb4df737933002a
SX-7 <sn1.test.preria.2002@gmail.com>
2024-01-21 22:10:49 +0100

    Finalised loader w/ error handling (general)

--- lib/blocs/form_blocs/receipt_form_block.dart
+++ lib/blocs/form_blocs/receipt_form_block.dart
@@ -6,6 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:budget_master/models/product.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_tesseract_ocr/android_ios.dart';
 import 'package:fuzzywuzzy/fuzzywuzzy.dart';
@@ -60,66 +61,84 @@ class ReceiptFormBloc extends FormBloc<String, String> {
           n_res.add(item);
         }
       }
-      // Asuuming bounds
-      var top = extractOne(query: 'FISKALNY', choices: n_res, cutoff: 75)
-          .index; // Lower cutoff due to 'paragon niefiskalny'
-      var bot = extractOne(query: 'PLN', choices: n_res, cutoff: 80)
-          .index; // Should be at the end of the list
-      for (var i = bot; i > top + 1; i--) {
-        if (n_res[i].contains("x") || n_res[i].contains("*")) {
-          bot = i;
-          print(n_res[i]);
-          break; // finds lowest multiplication sign, seems to be the only consistent delimiter
-        }
+      var results;
+      try {
+        results = extractText(n_res);
+      } catch (e) {
+        Fluttertoast.showToast(
+        msg: "Failed to process image",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black12,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
       }
-      for (var i = top + 1; i <= bot; i++) {
-        //TODO:
-        // Go line by line, remember that line with "*"/"x" is the last in pairs/singles
-        // If line doesn't have "*"/"x", look ahead and see if the next line is mostly numbers
-        // Actually do it always
-        // In case of any inconsistencies, probably just throw an error rn until we manage to error-catch it properly
-        // Which (todo) isn't done yet
-        var value = "v_test";
-        var name = "n_test";
-        if (i + 1 < n_res.length) {
-          var letters = 0;
-          var numbers = 0;
-          for (var char in n_res[i+1].characters){
-            if(RegExp('[a-zA-Z]').hasMatch(char))
-            {
-             letters +=1; 
-            }
-            else if (RegExp('\\d').hasMatch(char)){
-              numbers+=1;
-            } 
-          }
-          print(letters);
-          print(numbers);
-          if (numbers>letters) {
-            if (RegExp('[x*]').hasMatch(n_res[i+1])) {
-              value = n_res[i+1];
-              name = n_res[i];
-              i+=1;
-            } else if (RegExp('[x*]').hasMatch(n_res[i])) {
-              name = RegExp('.*\d[\s\d]+[x*].*').firstMatch(n_res[i]) as String;
-              value = RegExp('\d[\s\d]+[x*].*').firstMatch(n_res[i]) as String;
-              value += n_res[i+1].replaceAll('\n', ' ');
-              i+=1;
-            }
-          }
-        } else if (RegExp('[x*]').hasMatch(n_res[i])) {
-          name = RegExp('.*\d[\s\d]+[x*].*').firstMatch(n_res[i]) as String;
-          value = RegExp('\d[\s\d]+[x*].*').firstMatch(n_res[i]) as String;
-        }
+      
+      for (var result in results??[]) {
         products.addFieldBloc(ProductFieldBloc(
             productName:
-                TextFieldBloc(name: 'productName', initialValue: name),
-            price: TextFieldBloc(name: 'price', initialValue: value)));
+                TextFieldBloc(name: 'productName', initialValue: result.$1),
+            price: TextFieldBloc(name: 'price', initialValue: result.$2)));
       }
     }
   }
 
-  void extractText(String text) {}
+  List<(String, String)> extractText(List<String> text) {
+    // Asuuming bounds
+    var top = extractOne(query: 'FISKALNY', choices: text, cutoff: 75)
+        .index; // Lower cutoff due to 'paragon niefiskalny'
+    var bot = extractOne(query: 'PLN', choices: text, cutoff: 80)
+        .index; // Should be at the end of the list
+    for (var i = bot; i > top + 1; i--) {
+      if (text[i].contains("x") || text[i].contains("*")) {
+        bot = i;
+        break; // finds lowest multiplication sign, seems to be the only consistent delimiter
+      }
+    }
+    List<(String, String)> re_val = [];
+    for (var i = top + 1; i <= bot; i++) {
+      // In case of any inconsistencies, probably just throw an error rn until we manage to error-catch it properly
+      var value = "v_test";
+      var name = "n_test";
+      if (i + 1 < text.length) {
+        // if there is a next record, check if it's text or possible price data
+        var letters = 0;
+        var numbers = 0;
+        for (var char in text[i + 1].characters) {
+          if (RegExp('[a-zA-Z]').hasMatch(char)) {
+            letters += 1;
+          } else if (RegExp('\\d').hasMatch(char)) {
+            numbers += 1;
+          }
+        }
+        if (numbers > letters) {
+          // if the next record has numbers, check where the multiplication sign is, and merge
+          // consume next line by advancing iterator
+          if (RegExp('[x*]').hasMatch(text[i + 1])) {
+            value = text[i + 1];
+            name = text[i];
+            i += 1;
+          } else if (RegExp('[x*]').hasMatch(text[i])) {
+            name = RegExp('.*\d[\s\d]+[x*].*').firstMatch(text[i]) as String;
+            value = RegExp('\d[\s\d]+[x*].*').firstMatch(text[i]) as String;
+            value += text[i + 1].replaceAll('\n', ' ');
+            i += 1;
+          }
+        } else if (RegExp('[x*]').hasMatch(text[i])) {
+          name = RegExp('.*\d[\s\d]+[x*].*').firstMatch(text[i]) as String;
+          value = RegExp('\d[\s\d]+[x*].*').firstMatch(text[i]) as String;
+        }
+      } else if (RegExp('[x*]').hasMatch(text[i])) {
+        // last record check on single line product lists
+        name = RegExp('.*\d[\s\d]+[x*].*').firstMatch(text[i]) as String;
+        value = RegExp('\d[\s\d]+[x*].*').firstMatch(text[i]) as String;
+      }
+      re_val.add((name, value));
+    }
+    return re_val;
+  }
 
   void addProduct() {
     products.addFieldBloc(ProductFieldBloc(
